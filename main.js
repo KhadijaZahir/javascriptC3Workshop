b2a52debfb847d1eb959c067aac367bbee64e8b3
KhadijaZahir <57948334+KhadijaZahir@users.noreply.github.com>
2020-03-10 16:27:52 +0100

    finished workshop_1
    function addFavoriteBook(bookName){
        var favoriteBooks = ["c", "a", "k", "Great"];
        var bookName = favoriteBooks.includes("Great");
        if(bookName == false){
            favoriteBooks.push("Great");
        } else{
            console.log("there's the word Great")
        }
        console.log(favoriteBooks)
        function printFavoriteBooks(){
            // var favoriteBooks = ["c", "a", "k"];
            var N = favoriteBooks.length;
            console.log("Livres" + " " + "favoris" + " " + N)
    
             for(let favoriteBook of favoriteBooks) {
                console.log(favoriteBook);
            }
        }
    
        printFavoriteBooks();
    }
    addFavoriteBook();

---
+++ main.js
@@ -0,0 +1,23 @@
+function addFavoriteBook(bookName){
+    var favoriteBooks = ["c", "a", "k", "Great"];
+    var bookName = favoriteBooks.includes("Great");
+    if(bookName == false){
+        favoriteBooks.push("Great");
+    } else{
+        console.log("there's the word Great")
+    }
+    console.log(favoriteBooks)
+    function printFavoriteBooks(){
+        // var favoriteBooks = ["c", "a", "k"];
+        var N = favoriteBooks.length;
+        console.log("Livres" + " " + "favoris" + " " + N)
+    
+         for(let favoriteBook of favoriteBooks) {
+            console.log(favoriteBook);
+        }
+    }
+    
+    printFavoriteBooks();
+}
+addFavoriteBook();
+
